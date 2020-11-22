bd1a7ba8af1175b65b6983d23309800ac14cab3a
Michal Hornicky <mhornick@redhat.com>
2020-11-22 09:00:06 +0100

    Fix checks, prevent double disputes

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,23 @@ impl TxType {
     }
 }
 
+#[derive(Debug, PartialOrd, PartialEq)]
+pub enum TxState {
+    New,
+    Processed,
+    Failed,
+    Disputed,
+    Resolved,
+    Chargeback,
+}
+
+impl Default for TxState {
+    fn default() -> Self {
+        TxState::New
+    }
+}
+
+
 // Would've used a internally tagged enum, but serde csv doesn't like it
 // https://serde.rs/enum-representations.html
 #[derive(Debug, Deserialize)]
@@ -48,6 +65,9 @@ pub struct Tx {
     #[serde(rename = "tx")]
     id: u32,
     amount: Option<Value>,
+
+    #[serde(skip_serializing, skip_deserializing)]
+    state: TxState,
 }
 
 #[derive(Debug, Default)]
@@ -73,22 +93,30 @@ impl State {
             self.apply_op(tx)
         }
     }
-    pub fn apply_tx(&mut self, tx: Tx) -> Result<()> {
+
+    pub fn apply_tx(&mut self, mut tx: Tx) -> Result<()> {
         let acc = self.accounts
             .entry(tx.client)
             .or_insert_with(|| Default::default());
+
         let amount = tx.amount.ok_or(Error::msg("Expected amount associated"))?;
+
         match tx.t {
             TxType::Deposit => {
                 acc.available += amount;
+
+                tx.state = TxState::Processed;
                 self.transactions.insert(tx.id, tx);
             }
             TxType::Withdrawal => {
                 let res = acc.available - amount;
                 if res < 0.0 {
+                    tx.state = TxState::Failed;
                     bail!("Invalid withdrawal, not enough funds");
+                } else {
+                    acc.available -= amount;
+                    tx.state = TxState::Processed
                 }
-                acc.available -= amount;
                 self.transactions.insert(tx.id, tx);
             }
             other => bail!("Invalid transaction: {:?}", other)
@@ -97,7 +125,6 @@ impl State {
     }
 
     pub fn apply_op(&mut self, op: Tx) -> Result<()> {
-
         let actual = self.transactions.get_mut(&op.id);
         let actual = actual.ok_or_else(|| Error::msg("Tx not found"))?;
         ensure!(actual.client == op.client, "Operation referencing tx of different client");
@@ -107,17 +134,26 @@ impl State {
 
         match &op.t {
             TxType::Dispute => {
+                ensure!(actual.t.is_tx(), "Attempting to dispute {:?}", actual.t);
+                // Dispute -> resolve -> dispute flow sounds possible, let's allow it
+                ensure!(actual.state == TxState::Processed || actual.state == TxState::Resolved,
+                 "Attempting to dispute tx in state: {:?}", actual.state);
+
+                actual.state = TxState::Disputed;
                 acc.held += amount;
                 acc.available -= amount;
             }
             TxType::Resolve => {
-                ensure!(acc.held > amount, "Client held funds missing");
+                ensure!(acc.held >= amount, "Client held funds missing");
 
+                actual.state = TxState::Resolved;
                 acc.held -= amount;
                 acc.available += amount;
             }
             TxType::Chargeback => {
-                ensure!(acc.held > amount, "Client held funds missing");
+                ensure!(acc.held >= amount, "Client held funds missing");
+
+                actual.state = TxState::Chargeback;
                 acc.held -= amount;
                 acc.locked = true;
             }
@@ -158,6 +194,7 @@ pub fn process_stream(r: impl Read) -> Result<State> {
         .has_headers(true)
         .flexible(true)
         .trim(csv::Trim::All)
+        .comment(Some(b'#'))
         .terminator(csv::Terminator::CRLF)
         .from_reader(r);
 
