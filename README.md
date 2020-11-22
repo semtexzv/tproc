f315a83267ac4d2a838e72f07eeffdd3416c3ac6
Michal Hornicky <mhornick@redhat.com>
2020-11-22 09:12:19 +0100

    More checks

---
+++ README.md
@@ -0,0 +1,4 @@
+## Transcation Processor
+Takes csv [input](./0.in.csv), interprets it as a stream of transactions, and applies
+them to an intermediate state, outputting it to stdout in the end. 
+
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::io::{Read, Write};
 pub type Value = f64;
 
 #[derive(Debug, Deserialize, PartialOrd, PartialEq)]
-pub enum TxType {
+pub enum EntryType {
     #[serde(rename = "deposit")]
     Deposit,
     #[serde(rename = "withdrawal")]
@@ -29,17 +29,17 @@ pub enum TxType {
     Chargeback,
 }
 
-impl TxType {
+impl EntryType {
     pub fn is_tx(&self) -> bool {
-        self == &TxType::Deposit || self == &TxType::Withdrawal
+        self == &EntryType::Deposit || self == &EntryType::Withdrawal
     }
     pub fn is_op(&self) -> bool {
-        self != &TxType::Deposit && self != &TxType::Withdrawal
+        self != &EntryType::Deposit && self != &EntryType::Withdrawal
     }
 }
 
 #[derive(Debug, PartialOrd, PartialEq)]
-pub enum TxState {
+pub enum EntryState {
     New,
     Processed,
     Failed,
@@ -48,26 +48,27 @@ pub enum TxState {
     Chargeback,
 }
 
-impl Default for TxState {
+impl Default for EntryState {
     fn default() -> Self {
-        TxState::New
+        EntryState::New
     }
 }
 
-
 // Would've used a internally tagged enum, but serde csv doesn't like it
 // https://serde.rs/enum-representations.html
+/// Entry is an element of input. It is either mutable transaction or an operation which manipulates
+/// another mutable transaction. For this purposes we represent them in a single struct
 #[derive(Debug, Deserialize)]
-pub struct Tx {
+pub struct Entry {
     #[serde(rename = "type")]
-    t: TxType,
+    typ: EntryType,
     client: u16,
     #[serde(rename = "tx")]
     id: u32,
     amount: Option<Value>,
 
     #[serde(skip_serializing, skip_deserializing)]
-    state: TxState,
+    state: EntryState,
 }
 
 #[derive(Debug, Default)]
@@ -82,78 +83,84 @@ pub struct State {
     accounts: HashMap<u16, Account>,
     // Replace with BTreeMap, and remove old transactions in order to keep memory low
     // (limited dispute window)
-    transactions: HashMap<u32, Tx>,
+    transactions: HashMap<u32, Entry>,
 }
 
 impl State {
-    pub fn apply(&mut self, tx: Tx) -> Result<()> {
-        if tx.t.is_tx() {
+    pub fn apply(&mut self, tx: Entry) -> Result<()> {
+        if tx.typ.is_tx() {
             self.apply_tx(tx)
         } else {
             self.apply_op(tx)
         }
     }
-
-    pub fn apply_tx(&mut self, mut tx: Tx) -> Result<()> {
+    // Apply a transaction to an account if possible, if not possible, which provides more information
+    pub fn apply_tx(&mut self, mut tx: Entry) -> Result<()> {
         let acc = self.accounts
             .entry(tx.client)
             .or_insert_with(|| Default::default());
 
         let amount = tx.amount.ok_or(Error::msg("Expected amount associated"))?;
 
-        match tx.t {
-            TxType::Deposit => {
+        match &tx.typ {
+            EntryType::Deposit => {
                 acc.available += amount;
 
-                tx.state = TxState::Processed;
+                tx.state = EntryState::Processed;
                 self.transactions.insert(tx.id, tx);
             }
-            TxType::Withdrawal => {
+            EntryType::Withdrawal => {
                 let res = acc.available - amount;
                 if res < 0.0 {
-                    tx.state = TxState::Failed;
+                    tx.state = EntryState::Failed;
                     bail!("Invalid withdrawal, not enough funds");
                 } else {
                     acc.available -= amount;
-                    tx.state = TxState::Processed
+                    tx.state = EntryState::Processed
                 }
                 self.transactions.insert(tx.id, tx);
             }
-            other => bail!("Invalid transaction: {:?}", other)
+            _ => bail!("Invalid transaction: {:?}", tx)
         }
         Ok(())
     }
 
-    pub fn apply_op(&mut self, op: Tx) -> Result<()> {
+    // Apply an operation to pre-existing transaction
+    pub fn apply_op(&mut self, op: Entry) -> Result<()> {
         let actual = self.transactions.get_mut(&op.id);
         let actual = actual.ok_or_else(|| Error::msg("Tx not found"))?;
-        ensure!(actual.client == op.client, "Operation referencing tx of different client");
+        ensure!(actual.client == op.client, "Operation referencing tx of a different client");
+
         let acc = self.accounts.get_mut(&op.client)
             .ok_or_else(|| Error::msg("Client missing"))?;
         let amount = actual.amount.ok_or_else(|| Error::msg("Missing amount"))?;
 
-        match &op.t {
-            TxType::Dispute => {
-                ensure!(actual.t.is_tx(), "Attempting to dispute {:?}", actual.t);
+        match &op.typ {
+            // Allowing disputes of both deposits and withdrawals for now, spec requires us to lock funds
+            // This seems weird from my position, but let's follow the spec and see from there
+            EntryType::Dispute => {
+                ensure!(actual.typ.is_tx(), "Attempting to dispute {:?}", actual.typ);
                 // Dispute -> resolve -> dispute flow sounds possible, let's allow it
-                ensure!(actual.state == TxState::Processed || actual.state == TxState::Resolved,
-                 "Attempting to dispute tx in state: {:?}", actual.state);
+                ensure!(actual.state == EntryState::Processed || actual.state == EntryState::Resolved,
+                    "Attempting to dispute tx in state: {:?}", actual.state);
 
-                actual.state = TxState::Disputed;
+                actual.state = EntryState::Disputed;
                 acc.held += amount;
                 acc.available -= amount;
             }
-            TxType::Resolve => {
+            EntryType::Resolve => {
                 ensure!(acc.held >= amount, "Client held funds missing");
+                ensure!(actual.state == EntryState::Disputed, "Only disputed transactions can be resolved");
 
-                actual.state = TxState::Resolved;
+                actual.state = EntryState::Resolved;
                 acc.held -= amount;
                 acc.available += amount;
             }
-            TxType::Chargeback => {
+            EntryType::Chargeback => {
                 ensure!(acc.held >= amount, "Client held funds missing");
+                ensure!(actual.state == EntryState::Disputed, "Only disputed transactions can be charged back");
 
-                actual.state = TxState::Chargeback;
+                actual.state = EntryState::Chargeback;
                 acc.held -= amount;
                 acc.locked = true;
             }
@@ -164,7 +171,7 @@ impl State {
 
 
     pub fn write(&self, w: impl Write) -> Result<()> {
-        // Different fields than our inner account repr,
+        // Different fields than our inner account repr, create local temp struct for output
         #[derive(Serialize)]
         struct AccountOut {
             client: u16,
@@ -199,7 +206,7 @@ pub fn process_stream(r: impl Read) -> Result<State> {
         .from_reader(r);
 
     for tx in rdr.deserialize() {
-        let tx: Tx = tx?;
+        let tx: Entry = tx?;
         if let Err(e) = state.apply(tx) {
             eprintln!("Error: {}", e);
         }
